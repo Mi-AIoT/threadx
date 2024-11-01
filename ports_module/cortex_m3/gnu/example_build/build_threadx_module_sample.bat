e3f101a310b73eea1f99881ed8f4b4275fa108b7
Huaqi Fang <578567190@qq.com>
2024-11-01 11:07:11 +0800

    Do threadx sample and module experiment using qemu-system-arm with cortex-m3 see Run.txt
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- ports_module/cortex_m3/gnu/example_build/build_threadx_module_sample.bat
+++ ports_module/cortex_m3/gnu/example_build/build_threadx_module_sample.bat
@@ -3,3 +3,4 @@ arm-none-eabi-gcc -c -g -mcpu=cortex-m3 -fpie -fno-plt -mno-pic-data-is-text-rel
 arm-none-eabi-gcc -c -g -mcpu=cortex-m3 -fpie -fno-plt -mno-pic-data-is-text-relative -msingle-pic-base -I..\inc -I..\..\..\..\common\inc -I..\..\..\..\common_modules\inc sample_threadx_module.c
 arm-none-eabi-ld -A cortex-m3 -T sample_threadx_module.ld txm_module_preamble.o gcc_setup.o sample_threadx_module.o -e _txm_module_thread_shell_entry txm.a -o sample_threadx_module.axf -M > sample_threadx_module.map 
 
+arm-none-eabi-objcopy.exe sample_threadx_module.axf -O binary sample_threadx_module.bin
--- ports_module/cortex_m3/gnu/example_build/build_threadx_sample.bat
+++ ports_module/cortex_m3/gnu/example_build/build_threadx_sample.bat
@@ -1,4 +1,4 @@
 arm-none-eabi-gcc -c -g -mcpu=cortex-m3 -mthumb -I..\inc -I..\..\..\..\common\inc sample_threadx.c
 arm-none-eabi-gcc -c -g -mcpu=cortex-m3 -mthumb tx_simulator_startup.S
 arm-none-eabi-gcc -c -g -mcpu=cortex-m3 -mthumb cortexm_crt0.S
-arm-none-eabi-ld -A cortex-m3 -ereset_handler -T sample_threadx.ld tx_simulator_startup.o cortexm_crt0.o sample_threadx.o tx.a libc.a -o sample_threadx.axf -M > sample_threadx.map
+arm-none-eabi-gcc -g -mcpu=cortex-m3 -mthumb -T sample_threadx.ld -ereset_handler -nostartfiles -o sample_threadx.out -Wl,-Map=sample_threadx.map  tx_simulator_startup.o cortexm_crt0.o sample_threadx.o tx.a -lc -o sample_threadx.axf
